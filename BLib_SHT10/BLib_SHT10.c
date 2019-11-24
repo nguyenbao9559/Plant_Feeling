dcbd4e8a542e4bb6d88d679577a6c9a7a3b6ff2b
Nguyen Quoc Bao <nguyenbao9559@gmail.com>
2019-11-24 23:54:25 +0700

    version 1.3 , added SHT10 library

---
+++ BLib_SHT10/BLib_SHT10.c
@@ -0,0 +1,288 @@
+#include "stm32f4xx_hal.h"
+#include "BLib_SHT10.h"
+
+/*
+	This function is used to set the type of Hardware pin (input/output)
+*/
+//#define	SHT10_Power_Supply		3
+//#define SHT10_Resolution			0
+
+static float TempVal; // real temperature value of environment
+SHT10_Init_t SHT10_Init;
+
+void BLib_SHT10_Init(GPIO_TypeDef* GPIOx, uint16_t Data_Pin , uint16_t Clk_Pin , uint8_t SHT10_resolution , uint8_t SHT10_Power)
+{
+	SHT10_Init.SHT10_Resolution = SHT10_resolution;
+	SHT10_Init.SHT10_Power_Supply = SHT10_Power;
+	SHT10_Init.SHT10_DATA_PIN = Data_Pin;
+	SHT10_Init.SHT10_CLK_PIN = Clk_Pin;
+	SHT10_Init.SHT10_CLK_PORT = GPIOx;
+	SHT10_Init.SHT10_DATA_PORT = GPIOx;
+	if(SHT10_Init.SHT10_Power_Supply == 3)
+	{
+		SHT10_Init.d1 = -39.6;
+	}
+	if(SHT10_Init.SHT10_Power_Supply == 5)
+	{
+		SHT10_Init.d1 = -40.1;
+	}	
+	if(SHT10_Init.SHT10_Resolution == 0)
+	{
+		BLib_SHT10_StRegWrite(BLib_GlbSrv_CLr_Bit(BLib_SHT10_StRegRead() , SHT10_MeasReso_BP));
+		SHT10_Init.d2 = 0.01;
+		SHT10_Init.t1 = 0.01;
+		SHT10_Init.t2 = 0.00008;
+		SHT10_Init.c1 = -2.0468;
+		SHT10_Init.c2 = 0.0367;
+		SHT10_Init.c3 = -0.0000015955;
+	}
+	else
+	{
+		BLib_SHT10_StRegWrite(BLib_GlbSrv_Set_Bit(BLib_SHT10_StRegRead() , SHT10_MeasReso_BP));
+		SHT10_Init.d2 = 0.04;
+		SHT10_Init.t1 = 0.01;
+		SHT10_Init.t2 = 0.00128;
+		SHT10_Init.c1 = -2.0468;
+		SHT10_Init.c2 = 0.5872;
+		SHT10_Init.c3 = -0.00040845;
+	}
+}
+
+
+void BLib_MODER_Update(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, uint8_t Pin_Typ)
+{
+	GPIO_InitTypeDef GPIO_InitStruct;
+	if(Pin_Typ == Pin_Typ_Out)
+	{
+		GPIO_InitStruct.Pin   = GPIO_Pin;
+		GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP; // Push-pull output
+		GPIO_InitStruct.Pull  = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+		HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);	
+	}
+	else
+	{
+		GPIO_InitStruct.Pin   = GPIO_Pin;
+		GPIO_InitStruct.Mode  = GPIO_MODE_INPUT;
+		GPIO_InitStruct.Pull  = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+		HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);	
+	}
+}
+
+/*
+	This function is used to start commnication with SHT10 , the process of SHT10 communication is that 
+	Start communication ---> Sending command ---> wait for DATA ready ---> 00 MSB ---> ACK ---> LSB ---> ACK --->checksum ---> ACK	
+	Therefore , this function is first used in commnication process.
+*                           _____         ________  
+*                   DATA:        |_______|  
+*                               ___     ___  
+*                   SCK :   ___|   |___|   |______  
+*/
+void BLib_SHT10_Com_Strt(void)
+{
+	BLib_MODER_Update(SHT10_Init.SHT10_DATA_PORT , SHT10_Init.SHT10_DATA_PIN , Pin_Typ_Out); // Set DATA Pin as Output 	
+	HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_RESET);	
+	HAL_GPIO_WritePin(SHT10_Init.SHT10_DATA_PORT ,SHT10_Init.SHT10_DATA_PIN , GPIO_PIN_SET);
+	HAL_Delay(1); 
+	HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_SET);
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(SHT10_Init.SHT10_DATA_PORT ,SHT10_Init.SHT10_DATA_PIN , GPIO_PIN_RESET);
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_RESET);
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_SET);
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(SHT10_Init.SHT10_DATA_PORT ,SHT10_Init.SHT10_DATA_PIN , GPIO_PIN_SET);
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_RESET);
+	HAL_Delay(1);	
+}
+
+/*
+	If communication with the device is lost the below signal sequence will reset the serial interface
+	While leaving DATA high, toggle SCK nine or more times. This sequence resets the interface only:
+*                   communication reset: DATA-line=1 and at least 9 SCK cycles followed by transstart  
+*                     _____________________________________________________         ________  
+*               DATA:                                                      |_______|  
+*                        _    _    _    _    _    _    _    _    _        ___     ___  
+*               SCK : __| |__| |__| |__| |__| |__| |__| |__| |__| |______|   |___|   |______  	
+*/
+
+void BLib_SHT10_Com_Rst(void)
+{
+	BLib_MODER_Update(SHT10_Init.SHT10_DATA_PORT , SHT10_Init.SHT10_DATA_PIN , Pin_Typ_Out); // Set DATA Pin as Output
+	HAL_GPIO_WritePin(SHT10_Init.SHT10_DATA_PORT ,SHT10_Init.SHT10_DATA_PIN , GPIO_PIN_SET);
+	HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_RESET);
+	int i;
+	for(i=0; i < 9;i++)
+	{
+		HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_SET);
+		HAL_Delay(1);
+		HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_RESET);
+		HAL_Delay(1);
+		
+	}
+	BLib_SHT10_Com_Strt();
+	
+}
+
+uint8_t BLib_SHT10_Com_CmdWrite(uint8_t command)
+{
+	uint8_t i;
+	uint8_t Ack_Loc;
+	BLib_MODER_Update(SHT10_Init.SHT10_DATA_PORT , SHT10_Init.SHT10_DATA_PIN , Pin_Typ_Out); // Set DATA Pin as Output
+	for(i=0x80; i > 0;i/=2)
+	{
+		if(command & i)
+		{
+			HAL_GPIO_WritePin(SHT10_Init.SHT10_DATA_PORT ,SHT10_Init.SHT10_DATA_PIN , GPIO_PIN_SET);
+		}
+		else
+		{			
+			HAL_GPIO_WritePin(SHT10_Init.SHT10_DATA_PORT ,SHT10_Init.SHT10_DATA_PIN , GPIO_PIN_RESET);			
+		}
+		HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_SET);
+		HAL_Delay(1);
+		HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_RESET);
+		HAL_Delay(1);
+	}
+	BLib_MODER_Update(SHT10_Init.SHT10_DATA_PORT , SHT10_Init.SHT10_DATA_PIN , Pin_Typ_In); // Set DATA Pin as Input
+	if(!HAL_GPIO_ReadPin(SHT10_Init.SHT10_DATA_PORT , SHT10_Init.SHT10_DATA_PIN))
+	{
+		Ack_Loc = SHT10_ACK;
+	}
+	else
+	{
+		Ack_Loc = SHT10_nonACK;
+	}
+	HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_SET);
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_RESET);
+	HAL_Delay(1);
+	return Ack_Loc;
+}
+
+uint16_t BLib_SHT10_Com_DatRead(uint8_t Ack)
+{
+	uint8_t i;
+	uint8_t Dat_Loc = 0;
+	BLib_MODER_Update(SHT10_Init.SHT10_DATA_PORT , SHT10_Init.SHT10_DATA_PIN , Pin_Typ_In); // Set DATA Pin as Input
+	HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_RESET);	
+	for(i=0x80; i > 0; i/=2)
+	{
+		if(HAL_GPIO_ReadPin(SHT10_Init.SHT10_DATA_PORT , SHT10_Init.SHT10_DATA_PIN))
+		{	
+			Dat_Loc += i;
+		}
+		HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_SET);
+		HAL_Delay(1);
+		HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_RESET);
+		HAL_Delay(1);
+	}
+	if(Ack == SHT10_ACK)
+	{
+		BLib_MODER_Update(SHT10_Init.SHT10_DATA_PORT , SHT10_Init.SHT10_DATA_PIN , Pin_Typ_Out); // Set DATA Pin as Output
+		HAL_GPIO_WritePin(SHT10_Init.SHT10_DATA_PORT ,SHT10_Init.SHT10_DATA_PIN , GPIO_PIN_RESET);		
+		HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_SET);
+		HAL_Delay(1);		
+		HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_RESET);
+		HAL_Delay(1);		
+	}
+		if(Ack == SHT10_nonACK)
+	{
+		BLib_MODER_Update(SHT10_Init.SHT10_DATA_PORT , SHT10_Init.SHT10_DATA_PIN , Pin_Typ_Out); // Set DATA Pin as Output
+		HAL_GPIO_WritePin(SHT10_Init.SHT10_DATA_PORT ,SHT10_Init.SHT10_DATA_PIN , GPIO_PIN_SET);		
+		HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_SET);
+		HAL_Delay(1);		
+		HAL_GPIO_WritePin(SHT10_Init.SHT10_CLK_PORT ,SHT10_Init.SHT10_CLK_PIN , GPIO_PIN_RESET);
+		HAL_Delay(1);		
+	}
+	return Dat_Loc;
+}
+
+uint16_t BLib_SHT10_TempRaw_DdRead()
+{
+	uint8_t MSB_loc , LSB_loc , CRC_Loc;
+	uint16_t TempRaw_Loc = 0;
+	BLib_SHT10_Com_Strt();
+	BLib_SHT10_Com_CmdWrite(TempRead);
+	while(HAL_GPIO_ReadPin(SHT10_Init.SHT10_DATA_PORT ,SHT10_Init.SHT10_DATA_PIN));
+	MSB_loc = BLib_SHT10_Com_DatRead(SHT10_ACK);
+	LSB_loc = BLib_SHT10_Com_DatRead(SHT10_ACK);
+	CRC_Loc = BLib_SHT10_Com_DatRead(SHT10_nonACK);
+	TempRaw_Loc = (MSB_loc << 8) | LSB_loc;
+	return TempRaw_Loc;
+}
+
+uint16_t BLib_SHT10_HumiRaw_DdRead()
+{
+	uint8_t MSB_loc , LSB_loc , CRC_Loc;
+	uint16_t HumiRaw_Loc = 0;
+	BLib_SHT10_Com_Strt();
+	BLib_SHT10_Com_CmdWrite(HumiRead);
+	while(HAL_GPIO_ReadPin(SHT10_Init.SHT10_DATA_PORT ,SHT10_Init.SHT10_DATA_PIN));
+	MSB_loc = BLib_SHT10_Com_DatRead(SHT10_ACK);
+	LSB_loc = BLib_SHT10_Com_DatRead(SHT10_ACK);
+	CRC_Loc = BLib_SHT10_Com_DatRead(SHT10_nonACK);
+	HumiRaw_Loc = (MSB_loc << 8) | LSB_loc;
+	return HumiRaw_Loc;
+}
+
+float BLib_SHT10_HumiRead()
+{
+	uint16_t HuminRaw_Loc;
+	float Humi_Loc;
+	HuminRaw_Loc = BLib_SHT10_HumiRaw_DdRead();
+	Humi_Loc = SHT10_Init.c1 + SHT10_Init.c2*HuminRaw_Loc + SHT10_Init.c3*HuminRaw_Loc*HuminRaw_Loc; // Convert raw value of humidity to RH
+	Humi_Loc = (TempVal - 25.0)*(SHT10_Init.t1 + SHT10_Init.t2*HuminRaw_Loc) + Humi_Loc; // Temperature compensation, when temp is high, Humidity will be low
+	if(Humi_Loc > 90)
+	return Humi_Loc;
+}
+
+float BLib_SHT10_TempRead()
+{
+	uint16_t TempRaw_Loc;
+	float Temp_Loc;
+	TempRaw_Loc = BLib_SHT10_TempRaw_DdRead();
+	Temp_Loc = SHT10_Init.d1 + SHT10_Init.d2*TempRaw_Loc;
+	TempVal = Temp_Loc;
+	return Temp_Loc;
+}
+
+uint8_t BLib_SHT10_StRegRead()
+{
+	uint8_t CRC_Loc;
+	uint8_t StReg_Loc;
+	BLib_SHT10_Com_Strt();
+	BLib_SHT10_Com_CmdWrite(stRegRead);
+	StReg_Loc = BLib_SHT10_Com_DatRead(SHT10_ACK);
+	CRC_Loc = BLib_SHT10_Com_DatRead(SHT10_nonACK);
+	return StReg_Loc;	
+}
+
+void BLib_SHT10_StRegWrite(uint8_t Data_Reg)
+{
+	BLib_SHT10_Com_Strt();
+	BLib_SHT10_Com_CmdWrite(stRegWrte);
+	BLib_SHT10_Com_CmdWrite(Data_Reg);
+}
+
+void BLib_SHT10_SelfHeat()
+{
+	uint8_t Data_Reg;
+	Data_Reg = BLib_GlbSrv_Set_Bit(BLib_SHT10_StRegRead() , SHT10_Heater_BP);
+	BLib_SHT10_Com_Strt();
+	BLib_SHT10_Com_CmdWrite(stRegWrte);
+	BLib_SHT10_Com_CmdWrite(Data_Reg);
+}
+
+uint32_t BLib_GlbSrv_Set_Bit(uint32_t Data_Reg , uint8_t BP)
+{
+	return (Data_Reg | (1 << BP));
+}
+
+uint32_t BLib_GlbSrv_CLr_Bit(uint32_t Data_Reg , uint8_t BP)
+{
+	return (Data_Reg & (~(1 << BP)));
+}
---
+++ BLib_SHT10/BLib_SHT10.h
@@ -0,0 +1,114 @@
+#include "stm32f4xx_hal.h"
+
+#ifndef __BLib_SHT10_H__
+#define __BLib_SHT10_H__
+
+typedef struct
+{
+	uint16_t SHT10_DATA_PIN;
+	uint16_t SHT10_CLK_PIN;
+	GPIO_TypeDef* SHT10_DATA_PORT;
+	GPIO_TypeDef* SHT10_CLK_PORT;
+	uint8_t SHT10_Resolution;
+	uint8_t SHT10_Power_Supply;
+	float d1;
+	float d2;
+	float t1;
+	float t2;
+	float c1;
+	float c2;
+	float c3;
+}SHT10_Init_t;
+
+/***********Command to communicate with SHT10*********************************************************/
+#define TempRead		0x03 // 3 first bit is Address BP , last 5 BP is the command to read temperature
+#define HumiRead 		0x05 // 3 first bit is Address BP , last 5 BP is the command to read Humidity
+#define stRegRead		0x07 // Read from status register
+#define stRegWrte		0x06 // Write to status register 
+#define	sftRst			0x1E // Resets the interface, clears the status register to default values. Wait minimum 11 ms before next command
+
+
+
+	
+
+/***********SHT10 Communication configuration**********************************************************/
+//#define SHT10_DATA_PORT		GPIOD
+//#define SHT10_CLK_PORT		GPIOD
+//#define SHT10_DATA_PIN		GPIO_PIN_10
+//#define SHT10_CLK_PIN			GPIO_PIN_11
+
+#define Pin_Typ_In				1
+#define Pin_Typ_Out				0
+
+#define DATA_RisiFall_Tim	1 // the needed delay time (ns) for DATA rising / falling edge
+#define CLK_RisiFall_Tim	1 // the needed delay time (ns) for CLK rising / falling edge
+
+#define SHT10_ACK					1	
+#define SHT10_nonACK				0
+
+/***********SHT10 Status Register Bit position*********************************************************/
+
+#define SHT10_LowBatDect_BP			6 // will be activated when VDD < 2.47
+#define SHT10_MeasReso_BP				0 //'1’ = 8bit RH / 12bit Temp. resolution
+																	//’0’ = 12bit RH / 14bit Temp. resolution
+#define SHT10_Heater_BP					2 // The heater may increase the temperature of the sensor by 5 – 10°c beyond ambient temperature
+																	// o keep self heating below 0.1°C, SHT1x should not be active for more than 10% of the time – e.g. 
+																	//maximum one measurement per second at 12bit accuracy shall be made
+							  
+							  
+/***********SHT10 Function call************************************************************************/				
+
+void BLib_SHT10_Init(GPIO_TypeDef* GPIOx, uint16_t Data_Pin , uint16_t Clk_Pin , uint8_t SHT10_resolution , uint8_t SHT10_Power);
+							  
+uint8_t BLib_SHT10_StRegRead();							
+
+void BLib_SHT10_StRegWrite(uint8_t Data_Reg);
+
+uint32_t BLib_GlbSrv_Set_Bit(uint32_t Data_Reg , uint8_t BP);
+
+uint32_t BLib_GlbSrv_CLr_Bit(uint32_t Data_Reg , uint8_t BP);
+
+void BLib_SHT10_SelfHeat();
+								
+void BLib_MODER_Update(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, uint8_t Pin_Typ);							  
+
+void BLib_SHT10_Com_Strt(void);
+
+/*
+	This function is used for soft reset when the communication gets error
+*/
+void BLib_SHT10_Com_Rst(void);
+
+/*
+	This function is used for select command to communicate with SHT10
+	#define TempRead	0b00000011 // 3 first bit is Address BP , last 5 BP is the command to read temperature
+	#define HumiRead 	0b00000101 // 3 first bit is Address BP , last 5 BP is the command to read Humidity
+	#define stRead		0b00000111 // Read from status register
+	#define stWrte		0b00000110 // Write to status register 
+	#define	sftRst		0b00011110 // Resets the interface, clears the status register to default values. Wait minimum 11 ms before next command
+*/
+uint8_t BLib_SHT10_Com_CmdWrite(uint8_t command);
+/*
+	This function is used for Reading data from SHT10 sensor	
+	Argument:
+		#define SHT10_ACK			1
+		#define SHT_nonACK			0
+*/
+uint16_t BLib_SHT10_Com_DatRead(uint8_t Ack);
+/*
+	This function is used to read the raw temperature from sensor
+*/
+uint16_t BLib_SHT10_TempRaw_DdRead();
+/*
+	This function is used to read the raw humidity from sensor
+*/
+uint16_t BLib_SHT10_HumiRaw_DdRead();
+/*
+	This function is used to read the humidity value
+*/
+float BLib_SHT10_HumiRead();
+/*
+	This function is used to read the temperature value
+*/
+float BLib_SHT10_TempRead();
+#endif
